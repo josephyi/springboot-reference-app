12d5fb1bc82ca05aed83cc66091eeef0acdb6abc
josephyi <dissonance@gmail.com>
2025-08-21 11:36:45 -0700

    feat: add structured logging

--- src/main/java/dev/josephyi/reference/example00002/KafkaService.java
+++ src/main/java/dev/josephyi/reference/example00002/KafkaService.java
@@ -1,23 +1,30 @@
 package dev.josephyi.reference.example00002;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaService {
-    
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaService.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
 
     public void sendMessage(String topic, String message) {
         kafkaTemplate.send(topic, message);
-        System.out.println("Sent message to topic: " + topic);
+        logger.info("Sent message to topic: " + topic);
     }
 
     @KafkaListener(topics = "test-topic", groupId = "my-group")
     public void consumeMessage(String message) {
-        System.out.println("Consumed message: " + message);
+        logger.info("Consumed message: " + message);
     }
 }
