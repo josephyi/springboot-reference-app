61e5edfac35d987f8bc69d2b77852e8cded4c98f
josephyi <dissonance@gmail.com>
2025-08-21 11:35:43 -0700

    feat: add actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
 	implementation("ch.qos.logback:logback-classic:1.5.18")
